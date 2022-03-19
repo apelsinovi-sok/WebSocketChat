46c4b25b15c8eca87c53b2479e091fc9bc460396
Boris Kramarovskiy <weslastam01819@gmail.com>
2022-03-19 09:10:44 +0300

    добавил фавикон и сделал мелкий рефакторинг

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ var upgrade = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func chatStart(s *server, w http.ResponseWriter, r *http.Request) {
+func chatStart(s *server, c *gin.Context) {
 	upgrade.CheckOrigin = func(r *http.Request) bool { return true }
-	ws, err := upgrade.Upgrade(w, r, nil)
+	ws, err := upgrade.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		return
 	}
@@ -33,14 +33,19 @@ func homePage(c *gin.Context) {
 	}
 }
 
+func faviconHandler(c *gin.Context) {
+	http.ServeFile(c.Writer, c.Request, "favicon.ico")
+}
+
 func main() {
 	var s = newServer()
 	go s.run()
 	fmt.Printf("HTTP Сервер запущен на порту %s\n", PORT)
 	g := gin.Default()
 	g.GET("/", homePage)
+	g.GET("/favicon.ico", faviconHandler)
 	g.GET("/ws", func(c *gin.Context) {
-		chatStart(s, c.Writer, c.Request)
+		chatStart(s, c)
 	})
 	err := g.Run(PORT)
 	if err != nil {
